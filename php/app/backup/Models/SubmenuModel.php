1571019345fbe5a3c4bdab9f779601c07eadadaf
gleyce-domhelder <142454112+gleyce-domhelder@users.noreply.github.com>
2024-05-13 16:54:27 -0300

    Delete php directory

--- php/app/backup/Models/SubmenuModel.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Models;
-use CodeIgniter\Model;
-
-class SubmenuModel extends Model{
-    protected $table = 'SubMenu';
-    protected $primaryKey = 'idSubmenu';
-    protected $allowedFields = ['idSubmenu', 'Nome', 'idRoutes', 'idMenu'];
-}
